468eba73c67505dd8a1fff5240d01305cc61a741
Rousseau Alexandre <contact@rousseau-alexandre.Fr>
2018-10-12 14:45:35 +0200

    Organize in project

--- organizer.js
+++ organizer.js
@@ -2,9 +2,11 @@ const ExifImage = require('exif').ExifImage;
 
 const fs = require("fs")
 const path = require("path")
-const gpsUtil = require('gps-util');
+const gpsUtil = require('gps-util')
+const mv = require('mv');
+const sanitize = require("sanitize-filename")
 
-
+const NEAR_AREA = 50
 
 const projectsData = fs.readFileSync('data.json')
 
@@ -14,30 +16,71 @@ if (!projectsData) {
 
 const projects = JSON.parse(projectsData)
 
+/**
+ * Find project who match to GPS data
+ * @param  {float} latitude
+ * @param  {float} longitude
+ * @return {Project|null}           [description]
+ */
+function findProject(latitude, longitude) {
+  return projects.filter(project => project.latitude && project.longitude)
+    .find(project => {
+      let distance = gpsUtil.getDistance(longitude, latitude, project.longitude, project.latitude)
+      return distance < NEAR_AREA
+    })
+}
 
-
+/**
+ * [organizeImage description]
+ * @param  {String} imagePath
+ * @param  {Object} exifData
+ * @return {Boolean}
+ */
 function organizeImage(imagePath, exifData) {
-
+  // convert longitude
   let logitudeDms = exifData.gps.GPSLongitude
   let longitude = gpsUtil.toDD(logitudeDms[0], logitudeDms[1], logitudeDms[2])
-
+  // convert latitude
   let latitudeDms = exifData.gps.GPSLatitude
   let latitude = gpsUtil.toDD(latitudeDms[0], latitudeDms[1], latitudeDms[2])
 
-  projects.filter(project => project.latitude && project.longitude)
-    .forEach(project => {
-      let distance = gpsUtil.getDistance(longitude, latitude, project.longitude, project.latitude)
-      console.log(distance)
-    })
+  let project = findProject(latitude, longitude)
 
-  // gpsUtil.imageGpsInfo(imagePath, function(gps) {
-  //   console.log(gps)
-  // })
+  if (project === undefined) {
 
 
+    let newPath = path.join(
+      path.dirname(imagePath),
+      'not_found',
+      path.basename(imagePath)
+    )
+
+
+    return false
+  }
+
+  let newPath = path.join(
+    path.dirname(imagePath),
+    sanitize(project.name),
+    path.basename(imagePath)
+  )
+
+  mv(imagePath, newPath, {
+    mkdirp: true
+  }, error => {
+    if (error) {
+      return console.error(error);
+    }
+  })
+
+  return true
 }
 
-
+/**
+ * Will organize all file at the root of the given folder path
+ * @param  {String} imagesPath [description]
+ * @return {void}            [description]
+ */
 module.exports.run = function organizeImages(imagesPath) {
 
   fs.readdir(imagesPath, function(err, items) {
@@ -66,6 +109,5 @@ module.exports.run = function organizeImages(imagesPath) {
   })
 }
 
-
-
-// module.exports.run('/home/apprenant/Images')
+// for testing purpose
+// module.exports.run('/home/apprenant/Images/a')
