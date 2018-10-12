9c1e5288b78eed9ee7bf40d1af72ea3646738f47
Rousseau Alexandre <contact@rousseau-alexandre.Fr>
2018-10-12 10:28:59 +0200

    Setup electron

---
+++ app.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+const NodeGeocoder = require('node-geocoder')
+
+const geocoder = NodeGeocoder({
+  provider: 'openstreetmap',
+})
+
+const app = new Vue({
+  el: '#app',
+  // load JSON data
+  mounted: function() {
+    fs.readFile('data.json', (err, data) => {
+      if (err) {
+        return console.error(`cannot find database`)
+      }
+
+      this.projects = JSON.parse(data)
+    })
+  },
+  methods: {
+    // save vue app data as JSON
+    save: function(value) {
+      fs.writeFile('data.json', JSON.stringify(this.projects), (err) => {
+        if (err) {
+          return console.error(`cannot find database`)
+        }
+        console.log(`Saved :)`)
+      })
+    },
+    searchCoordinates: function(index) {
+      let app = this
+      let project = this.projects[index]
+      geocoder.geocode(project.address, function(geocodeErr, res) {
+
+        if (geocodeErr) {
+          return console.error(geocodeErr)
+        }
+
+        if (res.length === 0) {
+          return console.error(`Could not find data for ${project.address}`)
+        }
+
+        let firstRes = res[0]
+        console.log(`Coordinates resolved for ${project.address}`)
+
+        app.projects[index]['latitude'] = firstRes.latitude
+        app.projects[index]['longitude'] = firstRes.longitude
+        app.save()
+      });
+
+    }
+  },
+  data: {
+    projects: {},
+    selectedProject: null,
+  }
+})
--- index.js
+++ index.js
@@ -1,35 +1,55 @@
-const ExifImage = require('exif').ExifImage;
+const {
+  app,
+  BrowserWindow
+} = require('electron')
 
-const fs = require("fs")
-const path = require("path")
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
 
-
-const imagesPath = '/home/arousseau/Images/2018/italie/'
-
-
-const getPicturesWithGPS = () => new Promise((resolve, reject) => {})
-
-fs.readdir(imagesPath, function(err, items) {
-  if (err) {
-    return console.error(err)
-  }
-
-  items.forEach((item) => {
-    let itemPath = path.join(imagesPath, item)
-
-    new ExifImage({
-      image: itemPath
-    }, function(error, exifData) {
-      // handle error
-      if (error) {
-        return console.error(`Error with ${itemPath}: ${error.message}`)
-      }
-      // check GPS data
-      if (exifData.gps === undefined || Object.keys(exifData.gps).length === 0) {
-        return console.error(`Error with ${itemPath}: picture have no GPS data`)
-      }
-
-      console.log(exifData.gps); // Do something with your data!
-    })
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600
   })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
