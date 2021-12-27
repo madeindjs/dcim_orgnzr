876ee5b2dc527a14b2bab27b1915d67d4d276f59
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2021-12-27 22:39:12 +0100

    reorganize

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 // @ts-check
-const { loadConfiguration } = require("./configuration.js");
-const walk = require("walkdir");
-const path = require("path");
-const { getExifData } = require("./utils.js");
+const { getMovesForDirectory } = require("./organizer.js");
 
 const directoryPath = process.argv[2];
 
@@ -10,38 +7,4 @@ if (directoryPath === undefined) {
   throw Error("You must provide a directory");
 }
 
-async function main() {
-  const configuration = await loadConfiguration(directoryPath);
-
-  const haveExifRule = configuration.rules.some(({ property }) => property.startsWith("exif"));
-
-  /** @type {{from: string, to: string}[]} */
-  const moves = [];
-
-  let result = await walk.async(directoryPath, { return_object: true });
-
-  for (const file of Object.keys(result)) {
-    if (![".jpg"].includes(path.extname(file))) {
-      return;
-    }
-
-    if (haveExifRule) {
-      const exifData = await getExifData(file);
-      const createdDateRule = configuration.rules.find(({ property }) => property === "exif.CreateDate");
-
-      if (createdDateRule) {
-        const createdAt = exifData.exif.CreateDate;
-
-        moves.push({
-          from: file,
-          to: path.join(directoryPath, createdDateRule.destination.replace("$1", createdAt), path.basename(file)),
-        });
-        console.log(moves);
-      }
-    }
-  }
-
-  console.log(moves);
-}
-
-main().catch(console.error);
+getMovesForDirectory(directoryPath).then(console.log).catch(console.error);
